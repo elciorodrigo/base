97477ce491a7ea410d1195b8cff56cdd185f19a0
Rafael Galleani <rafegal@gmail.com>
2019-10-07 00:52:52 +0100

    crud de cliente, colaborador e ferramentas. Adicionado gitignore

--- app/views.py
+++ app/views.py
@@ -5,17 +5,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'app/index.html', {})  
 
-def employee(request):
-    return render(request, 'app/employee.html', {})  
-
-def employeeList(request):
-    return render(request, 'app/employee-list.html', {})  
-
 def customer(request):
-    return render(request, 'app/customer.html', {})  
+    return render(request, 'app/../crm/templates/customer.html', {})
 
 def customerList(request):
-    return render(request, 'app/customer-list.html', {})  
+    return render(request, 'app/../crm/templates/customer-list.html', {})
 
 def openContruction(request):
     return render(request, 'app/open-construction.html', {})  

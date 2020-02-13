c1c50a65ceffc8daff9462257ae1a50894dd573a
Diogo Almeida <diogo.rafa123@gmail.com>
2020-02-13 20:53:02 -0300

    Inicio contrato PDF

--- crm/views.py
+++ crm/views.py
@@ -7,7 +7,7 @@ from admin.settings import SITE_URL
 from .controller import set_employee, set_customer, set_tools, set_media, set_work, set_product, set_pay_order
 from crm.choices import PAY_ORDER_STATUS_FINISHED
 from crm.models import PayOrder, ProductWork
-from crm.util import render_to_pdf
+#from crm.util import render_to_pdf
 from .models import Employee, Position, Customer, Tools, Media, NoteCustomer, Work, EmployeeWork, Product
 from django.contrib.auth.decorators import login_required
 
@@ -255,7 +255,7 @@ def work_list(request):
 
 def generate_order_pdf(request):
     product = Product.objects.all()
-    pdf = render_to_pdf('contract.html', {})
+    #pdf = render_to_pdf('contract.html', {})
     return HttpResponse(pdf, content_type='application/pdf')
 
 
